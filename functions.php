6bde3e634db187a7bed5182d38419e37e1bc8db8
Tamás Horváth <htomy92@gmail.com>
2015-07-09 17:04:06 +0200

    Support custom post type

--- functions.php
+++ functions.php
@@ -1,16 +1,24 @@
 <?php
-   function pll_change_language_nav() {
+function pll_change_language_nav() {
     
     if ( function_exists('pll_languages_list') ) {
         
         global $wp_query;
         $postID = $wp_query->post->ID;
+        $postType = get_post_type($postID);
         $lang_list = array();
         
+        $postTypes = array("page", "post");
+        
         foreach ( pll_languages_list() as $code) {
-            $lang_list[$code] = get_post(pll_get_post($postID, $code))->post_name;
+            
+            if ( in_array($postType, $postTypes) ) {
+                $lang_list[$code] = get_post(pll_get_post($postID, $code))->post_name;
+            } else {
+                $lang_list[$code] = $postType . '/' . get_post(pll_get_post($postID, $code))->post_name;
+            }
         }
-
+        
         wp_enqueue_script('pll_nav', plugin_dir_url( __FILE__ ) . 'js/pll_change_language_nav.js');
         wp_localize_script('pll_nav', 'pllVars', array(
                 'postID' => $lang_list

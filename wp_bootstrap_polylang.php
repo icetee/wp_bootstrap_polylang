8c3c5ea989ce45f09e09bd18a7223b38436de21d
Tamás Horváth <htomy92@gmail.com>
2015-07-13 22:20:00 +0200

    Update 1.2.2 Version
    Fixed tag terms

--- wp_bootstrap_polylang.php
+++ wp_bootstrap_polylang.php
@@ -4,7 +4,7 @@
 *   Polylang Create Bootstrap Dropdown - WordPress
 *   Github: https://github.com/icetee/wp-bootstrap-polylang
 *
-*   @since 1.2
+*   @since 1.2.2
 *
 */
 
@@ -22,7 +22,13 @@ function wp_bootstrap_polylang() {
         foreach ( pll_languages_list() as $code) {
             if ( !is_front_page() ) {
                 if ( in_array($postType, $postTypes) ) {
-                    $lang_list[$code] = get_post(pll_get_post($postID, $code))->post_name;
+                    if ( !is_tag() ) {
+                        $lang_list[$code] = get_post(pll_get_post($postID, $code))->post_name;
+                    } else {
+                        $term_id = pll_get_term(get_the_tags(pll_set_term_language($postID, $code))[1]->term_id, $code);
+                        $lang_list[$code] = "tag/" . get_term_by('id', $term_id, 'post_tag')->slug;
+                    }
+                    
                 } else {
                     $lang_list[$code] = $postType . '/' . get_post(pll_get_post($postID, $code))->post_name;
                 }
@@ -31,7 +37,7 @@ function wp_bootstrap_polylang() {
             }
         }
 
-        wp_enqueue_script('pll_nav', get_template_directory_uri() . '/js/wp_bootstrap_polylang.js', array(), '1.2', true);
+        wp_enqueue_script('pll_nav', get_template_directory_uri() . '/js/wp_bootstrap_polylang.js', array(), '1.2.1', true);
         wp_localize_script('pll_nav', 'pllVars', array(
                 'postID' => $lang_list
             )
